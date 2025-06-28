2d3008eb20500149a329d83596c9b31cef22f4a8
whpz <2206373437@qq.om>
2025-06-28 14:54:07 +0800

    first commit

---
+++ src/test/java/org/example/campuscartrade/CampusCarTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.campuscartrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusCarTradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
