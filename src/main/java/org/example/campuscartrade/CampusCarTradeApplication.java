dd93abc025cf397579afd9403660dc6a533ca220
whpz <2206373437@qq.om>
2025-06-28 14:54:07 +0800

    first commit

---
+++ src/main/java/org/example/campuscartrade/CampusCarTradeApplication.java
@@ -0,0 +1,13 @@
+package org.example.campuscartrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCarTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusCarTradeApplication.class, args);
+    }
+
+}
